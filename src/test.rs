307544369b90408e505d3e36ca5e12408afac680
thud <jasper.parish@gmail.com>
2021-01-31 10:02:34 +0000

    Fix tests

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,4 @@
+use crate::obj::error::*;
 use crate::obj::requests::*;
 use crate::obj::responses::*;
 
@@ -11,17 +12,34 @@ fn get_api_keys() -> (String, String) {
 }
 
 #[test]
-fn test_api_blogentry() {
+fn test_api_bad_blogentry() {
     let (k, s) = get_api_keys();
     let x = CFBlogEntryCommand::Comments { blog_entry_id: -1 };
-    println!("{:?}", x.get(&k, &s));
+    match x.get(&k, &s) {
+        Err(Error::CodeforcesApi(e)) => {
+            println!("Received expected error: {}", e);
+        }
+        _ => {
+            panic!("Fail, Expected error from Codeforces Api.");
+        }
+    }
 }
 
 #[test]
 fn test_api_user() {
     let (k, s) = get_api_keys();
     let x = CFUserCommand::Friends { only_online: None };
-    println!("{:?}", x.get(&k, &s));
+    match x.get(&k, &s) {
+        Ok(CFResult::CFFriends(v)) => {
+            println!("Received friends list successfully: {:?}", v);
+        }
+        Ok(_) => {
+            panic!("Fail, user.friends response not parsed into Vec<String>");
+        }
+        Err(e) => {
+            panic!("Fail, request failed: {:?}", e);
+        }
+    }
 }
 
 #[test]
@@ -32,7 +50,22 @@ fn test_api_user_status() {
         from: None,
         count: Some(3),
     };
-    println!("{:?}", x.get(&k, &s));
+    match x.get(&k, &s) {
+        Ok(CFResult::CFSubmissionVec(v)) => {
+            println!(
+                "Received user submissions (user.status) successfully: {:?}",
+                v
+            );
+        }
+        Ok(_) => {
+            panic!(
+                "Fail, user.status response not parsed into CFSubmissionVec"
+            );
+        }
+        Err(e) => {
+            panic!("Fail, request failed: {:?}", e);
+        }
+    }
 }
 
 #[test]
@@ -46,15 +79,41 @@ fn test_api_problem() {
         room: None,
         show_unofficial: Some(false),
     };
-    let y = x.get(&k, &s);
-    println!("y = {:?}", y);
-    if let Ok(CFResult::CFContestStandings(CFContestStandings {
-        mut problems,
-        ..
-    })) = y
-    {
-        let p = &mut problems[5];
-        println!("{:?}", p.fetch_testcases());
+    match x.get(&k, &s) {
+        Ok(CFResult::CFContestStandings(d)) => {
+            println!("Received contest standings (contest.standings) successfully: {:?}", d);
+        }
+        Ok(_) => {
+            panic!(
+                "Fail, contest.standings response not parsed into CFContestStandings"
+            );
+        }
+        Err(e) => {
+            panic!("Fail, request failed: {:?}", e);
+        }
     }
-    println!("{:?}", fetch_testcases_for_problem(&1200, &"F".to_string()));
+}
+
+#[test]
+fn test_fetch_testcase() {
+    let mut p = CFProblem {
+        contest_id: Some(1477),
+        problemset_name: None,
+        index: Some("B".to_string()),
+        name: "Nezzar and Binary String".to_string(),
+        problem_type: CFProblemType::Programming,
+        points: Some(1000.0),
+        rating: Some(1900),
+        tags: vec!["data structures".to_string(), "greedy".to_string()],
+        input_testcases: None,
+    };
+    match p.fetch_testcases() {
+        Ok(v) => {
+            assert!(p.input_testcases.unwrap().len() > 0);
+            println!("Received problem testcases successfully: {:?}", v);
+        }
+        Err(e) => {
+            panic!("Fail, testcase request failed: {:?}", e);
+        }
+    }
 }
